5754bd5531e801bc8d78dd77f95419b5f2ba0ffe
shweta-vengurlekar11 <63588827+shweta-vengurlekar11@users.noreply.github.com>
2020-10-20 22:33:56 +0530

    Update q3.cs

--- day 1/q3.cs
+++ day 1/q3.cs
@@ -1,15 +1,15 @@
-﻿//using System;
+using System;
 
-//namespace day1
-//{
-//    class q1
-//    {
-//        static void Main(string[] args)
-//        {
-//            float a = 26.35f;
-//            float sqr = 0;
-//            sqr=a*a;
-//            Console.WriteLine("sum:" + sqr);
-//        }
-//    }
-//}
+namespace day1
+{
+    class q1
+    {
+        static void Main(string[] args)
+        {
+            float a = 26.35f;
+            float sqr = 0;
+            sqr = a * a;
+            Console.WriteLine("sum:" + sqr);
+        }
+    }
+}
