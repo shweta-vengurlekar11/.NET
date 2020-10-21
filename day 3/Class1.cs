327d421f2068e3a0bf2aee6ec4725db76ea7ec1c
shweta-vengurlekar11 <63588827+shweta-vengurlekar11@users.noreply.github.com>
2020-10-22 01:37:27 +0530

    Add files via upload

---
+++ day 3/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace mycalculator
+{
+    public class mycalculatorclass
+    {
+        public int add(int x, int y)
+        {
+            return x + y;
+        }
+        public int product(int x, int y)
+        {
+            return x * y;
+        }
+        public int cube(int x)
+        {
+            return x * x*x;
+        }
+    }
+}
