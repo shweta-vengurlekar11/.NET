6132ed73c28d00e61483075e8d4ac8daf68f71b2
shweta-vengurlekar11 <63588827+shweta-vengurlekar11@users.noreply.github.com>
2020-11-02 23:41:42 +0530

    assignment solution

---
+++ day 13/q1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day_13
+{
+    public class Account
+    {
+        readonly int id;
+        String name;
+        double balanceamt;
+        static int getid = 0;
+        protected const int minname = 15, minbal = 1000;
+        static Account() { Console.WriteLine("ICICI Bank"); }
+
+        public Account(String nm, double bal)
+        {
+            id = ++getid;
+            Name = nm;
+            Balanceamt = bal;
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value.Length <= minname)
+                    name = value;
+                else
+                    throw new Exception("name length can not be more that 25");
+            }
+        }
+        public double Balanceamt
+        {
+            get
+            { return balanceamt; }
+            set
+            {
+                if (value <= 0)
+                    throw new Exception("balance amount can not be less than or equal to zero");
+                else
+                    balanceamt = value;
+            }
+        }
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+        public void deposit(double amt)
+        {
+            if (amt > 0)
+                balanceamt += amt;
+            else
+                throw new Exception("deposite amount should not be 0 or nagative");
+        }
+        public void withdraw(double amt)
+        {
+            if ((balanceamt-amt) < 1000)
+            {
+                throw new Exception("insuffiecient balance");
+            }
+            else
+                balanceamt -= amt;
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2}", Id, Name, Balanceamt);
+        }
+    }
+    class q1
+    {
+        static void Main()
+        {
+            Account a = new Account("shweta", 20000);
+            //Account a1 = new Account("shweta vengurlekar", 20000);
+
+            //a.Balanceamt=-36666;
+            //a.withdraw(19500);
+            Console.WriteLine(a.ToString());
+        }
+    }
+}
---
+++ day 13/q2.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+
+namespace day_13
+{
+    public class student
+    {
+        int id; string name; double marks;
+        static int getid = 0;
+        public student(string nm, double mks)
+        {
+            id = ++getid;
+            Name = nm;
+            Marks = mks;
+        }
+        public string Name
+        {
+            get; set;
+        }
+        public double Marks
+        {
+            get; set;
+        }
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+        public void give_gracemarks(int mks)
+        {
+            if (mks > 5)
+            {
+                throw new myexception("Grace Marks more than 5 is not allowed");
+            }
+            marks += mks;
+        }
+    }
+    class myexception : ApplicationException
+    {
+        public string msg;
+        public myexception(string msg)
+        {
+            this.msg = msg;
+        }
+    }
+    class q2
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+            FileStream fout = null;
+            StreamWriter fstr_out = null;
+
+            try
+            {
+                student s1 = new student("shweta", 10);
+                student s2 = new student("abs", 34);
+                s1.give_gracemarks(15);
+
+                fout = new FileStream("test.txt", FileMode.Create, FileAccess.Write);
+                fstr_out = new StreamWriter(fout);
+
+                str = s1.Id + " " + s1.Name + " " + s1.Marks;
+                fstr_out.Write(str);
+            }
+            catch (myexception e)
+            {
+                Console.WriteLine("{0}", e.msg);
+            }
+            catch (Exception e1)
+            {
+                Console.WriteLine("{0}", e1.Message);
+            }
+            finally
+            {
+                fstr_out.Close();
+            }
+
+        }
+    }
+}
