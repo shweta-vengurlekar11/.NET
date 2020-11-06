eb45d17dc9ea27b75966b9efa58dd97b5cffec04
shweta-vengurlekar11 <63588827+shweta-vengurlekar11@users.noreply.github.com>
2020-11-06 23:52:40 +0530

    Assignment Solution

---
+++ day 17/q2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ADO01_ConsoleApplication
+{
+    public class Employee
+    {
+        public void details()
+        {
+
+            string connectionString = @"Data Source=(localdb)\Projects;Initial Catalog=Empdata;Integrated Security=True";
+
+            using (SqlConnection con = new SqlConnection())//Idesposable class written inside 
+            //using block
+            {
+                con.ConnectionString = connectionString;
+                SqlCommand cmd = new SqlCommand("Select * from Emp", con);
+
+                con.Open();
+                Console.WriteLine("connected");
+                SqlDataReader rdr = cmd.ExecuteReader(); //returns object of sqldatareder
+                if (rdr.HasRows)
+                {
+                    while (rdr.Read())
+                    {
+
+                        Console.WriteLine("{0} {1} {2}", rdr["Id"], rdr["Name"], rdr["Salary"]);
+                    }
+                }
+            }
+
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Employee obj = new Employee();
+            obj.details();
+        }
+
+    }
+}
