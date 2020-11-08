e1cf06e173c7bf71b2ec4b17930962e6a69f8f70
shweta-vengurlekar11 <63588827+shweta-vengurlekar11@users.noreply.github.com>
2020-11-08 11:37:47 +0530

    Assignment Solutions

---
+++ day 17/q3.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day16
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public double Salary { get; set; }
+        public string Department { get; set; }
+
+        public Employee(int id, string nm, double sal, string dept)
+        {
+            Id = id;
+            Name = nm;
+            Salary = sal;
+            Department = dept;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2} {3}", Id, Name, Salary, Department);
+        }
+    }
+    class q1
+    {
+        static void Main(string[] args)
+        {
+            Employee[] e = new[]
+                {
+            new Employee(1, "shweta", 82000, "HR"),
+            new Employee(2, "vikas", 13000, "Developer"),
+            new Employee(3, "pqr", 28000, "Developer"),
+            new Employee(4, "lmn", 18000, "HR"),
+            new Employee(5,"viren",26000,"Back-office")
+        };
+
+            //foreach (Employee d in e)
+            //    Console.WriteLine(d);
+            e.ToList().ForEach(a => Console.WriteLine(a));
+
+            Console.WriteLine("---------------------");
+
+            var odername = from d in e group d by d.Department into g select g;
+            foreach (var o in odername)
+            {
+                Console.WriteLine(o.Key);
+                foreach (var a in o)
+                    Console.WriteLine("{0}", a);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("---------------------");
+
+            var odername1 = from d in e group d by d.Department into g select new { dept = g.Key, emp = g };
+            foreach (var o in odername1)
+            {
+                Console.WriteLine(o.dept);
+                foreach (var a in o.emp)
+                    Console.WriteLine("{0}", a);
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ day 17/q4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace day17
+{
+    class mythread
+    {
+        public Thread thrd;
+        public mythread(string name, int num)
+        {
+            thrd = new Thread(this.fact);
+            thrd.Name = name;
+            thrd.Start(num);
+        }
+        public void fact(Object num)
+        {
+            Console.WriteLine("current thread is: {0}", Thread.CurrentThread.Name);
+            int a,temp=1;
+            a=(int)num;
+            for (int i = 2; i <= a; i++)
+            {
+                temp *= i;
+            }
+            Console.WriteLine("factorial of {0} is {1}", a, temp);
+        }
+    }
+    class q4
+    {
+        static void Main()
+        {
+            mythread m = new mythread("1st thread", 5);
+            mythread m1 = new mythread("2nd thread", 10);
+        }
+    }
+}
